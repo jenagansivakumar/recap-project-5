0a451a4dca7ad82f7e3597118673a72cdfccb9e6
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-04-26 17:09:18 +0200

    added images

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import useSWR from "swr";
+import Image from "next/image";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 const URL = "https://example-apis.vercel.app/api/art";
@@ -10,11 +11,12 @@ export default function ArtPieceList() {
   return (
     <>
       <ul>
-        {data.map((art) => {
+        {data?.map((art) => {
           return (
             <li key={art.slug}>
               {art.name}
               {art.artist}
+              <Image src={art.imageSource} width={100} height={100} />
             </li>
           );
         })}
