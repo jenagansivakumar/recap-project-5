695c5cdee16064abd24fbd8e85c6c8ce4b35b8c2
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-04-26 18:01:48 +0200

    finished refactoring and art piece list

--- components/ArtPiecePreview.js
+++ components/ArtPiecePreview.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 export default function ArtPiecePreview({ image, title, artist, altTag }) {
+  console.table({ image, title, artist, altTag });
   return (
     <article>
       <p>{title}</p>
