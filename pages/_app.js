4df45b2c9eeaa94015c2df374e6fb6bcc019f2ca
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-27 14:15:44 +0200

    header testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import GlobalStyle from "../styles";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 
 export default function App({ Component, pageProps }) {
   return (
